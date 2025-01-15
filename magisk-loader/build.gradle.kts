8d07912d1832c78c9f58a73824a56f95eaab3860
ZQBCWG <109116270+ZQBCWG@users.noreply.github.com>
2025-01-15 17:58:44 +0800

    Forgot to import

--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -16,13 +16,25 @@
  *
  * Copyright (C) 2021 - 2022 LSPosed Contributors
  */
-
+import org.apache.tools.ant.filters.FixCrLfFilter
+import org.apache.commons.codec.binary.Hex
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.security.KeyFactory
+import java.security.KeyPairGenerator
+import java.security.Signature
+import java.security.interfaces.EdECPrivateKey
 import org.apache.commons.codec.binary.Hex
 import org.apache.tools.ant.filters.FixCrLfFilter
 import org.apache.tools.ant.filters.ReplaceTokens
 import java.io.ByteArrayOutputStream
 import java.security.MessageDigest
-
+import java.security.interfaces.EdECPublicKey
+import java.security.spec.EdECPrivateKeySpec
+import java.security.spec.NamedParameterSpec
+import java.util.TreeSet
+import android.databinding.tool.ext.capitalizeUS
+import java.security.MessageDigest
 plugins {
     alias(libs.plugins.agp.app)
     alias(libs.plugins.lsplugin.resopt)
